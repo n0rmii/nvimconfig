f49c1e034ccde57f2d7a1f7cbb7c45cbbe673f62
n0rmii <michal@zywer.pl>
2024-02-21 19:47:27 +0100

    cleanup before re-setup

--- after/plugin/telescope.lua
+++
@@ -1,3 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_files, {})
--- lua/neovimsetup/init.lua
+++
@@ -1,3 +0,0 @@
-require("neovimsetup.remap")
-require("neovimsetup.set")
-require("neovimsetup.packer")
