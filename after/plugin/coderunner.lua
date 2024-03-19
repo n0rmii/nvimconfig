2959a6599c0be92a4cfe33e0d8459fe8a09deb78
n0rmii <michal@zywer.pl>
2024-03-19 11:02:44 +0100

    python3 is not a windows command

--- after/plugin/coderunner.lua
+++ after/plugin/coderunner.lua
@@ -1,6 +1,6 @@
 require('code_runner').setup({
   filetype = {
-    python = "python3 -u",
+    python = "python -u",
 	asm = {
 		"nasm -fobj $fileName &&",
 		"alink -subsys console -oPE $fileNameWithoutExt.obj &&",
